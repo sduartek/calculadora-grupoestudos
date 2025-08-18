8520f72b1edbbf73773157a199bfaf42070b81b0
Sergio Duarte <duartesergiok@gmail.com>
2025-08-18 16:26:35 -0300

    falta % troca sinal

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 function inserir(num) {
-    document.querySelector(".tela").innerHTML = num;    
+    document.querySelector(".tela").innerHTML += num;
+}
+
+function clean() {
+    document.querySelector(".tela").innerHTML = "";
+}
+
+function limpaUm() {
+    let tela = document.querySelector(".tela").innerHTML;
+    document.querySelector(".tela").innerHTML = tela.slice(0, - 1);
+}
+
+function somar() {
+    let tela = document.querySelector(".tela").innerHTML;
+    document.querySelector(".tela").innerHTML = eval(tela);
+    if (tela) {
+        document.querySelector(".tela").innerHTML = eval(tela);
+    } else {
+        document.querySelector(".tela").innerHTML = "erro"
+    }
+
+    function mudarSinal(){
+        let tela = document.querySelector(".tela").innerHTML;
+        if (tela) {
+            
+        }
+
+    }
 }
